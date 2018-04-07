b89b1276252f45c65c4717a4f2a038fa6fc8cffd
maicolxd <38084534+maicolxd@users.noreply.github.com>
2018-04-06 21:18:42 -0500

    Update user-add.sh

--- user-add.sh
+++ user-add.sh
@@ -9,7 +9,7 @@ useradd -e `date -d "$masaaktif days" +"%Y-%m-%d"` -s /bin/false -M $Login
 exp="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
 echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
 echo -e ""
-echo -e "====Informasi SSH Account===="
+echo -e "====Informacion de SSH ======"
 echo -e "Host: $IP" 
 echo -e "Port OpenSSH   : 22,53"
 echo -e "Port Dropbear  : 80,443"
