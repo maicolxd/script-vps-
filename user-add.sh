d9bc1fd0cf1210b3cb6a3297e0e0a5a7b8c0d0f8
maicolxd <38084534+maicolxd@users.noreply.github.com>
2018-04-06 20:39:46 -0500

    Update user-add.sh

--- user-add.sh
+++ user-add.sh
@@ -2,14 +2,14 @@
 
 read -p "Username : " Login
 read -p "Password : " Pass
-read -p "Expira (VPS): " masaaktif
+read -p "Expired (hari): " masaaktif
 
 IP=`dig +short myip.opendns.com @resolver1.opendns.com`
 useradd -e `date -d "$masaaktif days" +"%Y-%m-%d"` -s /bin/false -M $Login
 exp="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
 echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
 echo -e ""
-echo -e "====Informacion SSH ===="
+echo -e "====Informasi SSH Account===="
 echo -e "Host: $IP" 
 echo -e "Port OpenSSH   : 22,53"
 echo -e "Port Dropbear  : 80,443"
@@ -18,7 +18,7 @@ echo -e "Port Squid     : 8080,3128"
 echo -e "Config OpenVPN (TCP 1194): http://$IP:81/client.ovpn"
 echo -e "Username: $Login "
 echo -e "Password: $Pass"
-echo -e "------------------------"
-echo -e "Grupo vip: $exp"
-echo -e "========================"
-echo -e "========~mscvip~========"
+echo -e "-----------------------------"
+echo -e "VPN VIP: $exp"
+echo -e "============================="
+echo -e "==========MSC================"
